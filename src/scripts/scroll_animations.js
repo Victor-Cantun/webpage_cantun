081a6ca992609b5f9f37df4fac8f20768349b7de
Victor-Cantun <90561168+Victor-Cantun@users.noreply.github.com>
2024-07-26 08:39:54 -0600

    version 1.2

--- src/scripts/scroll_animations.js
+++ src/scripts/scroll_animations.js
@@ -1,9 +1,3 @@
-ScrollReveal().reveal(".title", {
-  reset: true,
-  duration: 200,
-  distance: "100px",
-  origin: "bottom",
-});
 ScrollReveal().reveal(".header", {
   duration: 200,
   distance: "100px",
@@ -13,8 +7,6 @@ ScrollReveal().reveal(".text-2", {
   duration: 2000,
   scale: 0.5,
 });
-/* ScrollReveal().reveal(".text-1", { delay: 500, duration: 1000 });
-ScrollReveal().reveal(".text-3", { delay: 500, duration: 3000 }); */
 ScrollReveal().reveal(".text-1", {
   delay: 500,
   easing: "steps(5)",
@@ -27,7 +19,6 @@ ScrollReveal().reveal(".widget", {
   reset: true,
   interval: 200,
   origin: "right",
-  /* distance: "500px", */
 });
 ScrollReveal().reveal(".card_figure", {
   reset: true,
